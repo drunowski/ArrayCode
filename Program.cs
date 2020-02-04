9cb92c5190a366a70669802f4aca647d8093d15f
David Runowski <drunowski@usf.edu>
2020-02-04 17:13:24 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//Author: David runowski
+//Date: 02/06/2020
+using System;
+
+namespace ArrayCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] davidsArray = new int[25];//create davids array with length of 25
+            for (int i = 1; i <= 25; i++)//use for loop to fill davidsarray
+            {
+                davidsArray[i - 1] = i;//fill davidsarray at i-1 with i value
+
+            }
+            for (int i = 0; i < davidsArray.Length; i++)//use for loop to go through davidsArray
+            {
+                Console.WriteLine("Element Value= " + davidsArray[i]);//Print each element in davidsArray.
+            }
+
+        }
+
+        
+    }
+}
